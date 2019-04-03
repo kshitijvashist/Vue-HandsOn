f61a8d7fa3240b941de66fa26e8fe9af032ddf03
Kshitij Vashist <23kshitij92@gmail.com>
2019-04-03 23:36:40 +0200

    New Component In View

---
+++ src/Sports-dataApp.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <sports-data v-bind:sports="sports" />
+  
+  </div>
+</template>
+
+<script>
+import Sports from './components/sports-data.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Sports
+  },
+  data(){
+      return{
+          sports:'Football,Cricket'
+      }
+  }
+ 
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
